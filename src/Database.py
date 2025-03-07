a13b6d870d202b606486772a230f3a43603daa11
normchell <nashvlad370@gmail.com>
2025-03-07 19:18:50 +0300

    updated the order to mach db columns

--- src/Database.py
+++ src/Database.py
@@ -5,3 +5,22 @@ class Database:
         self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
 
+
+    def init_puzzles(self):
+        self.cur.execute('''
+            CREATE TABLE IF NOT EXISTS Puzzles (
+                id INTEGER PRIMARY KEY,
+                Site TEXT,
+                WhiteElo INTEGER,
+                BlackElo INTEGER,
+                White TEXT,
+                Black TEXT,
+                TimeControl TEXT,
+                fen TEXT NOT NULL,
+                movesBefore TEXT,
+                movesPuzzle TEXT NOT NULL,
+                movesAfter TEXT
+            )
+        ''')
+
+        self.con.commit()
